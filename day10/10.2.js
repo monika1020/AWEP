47a6e8c1616bfccedf714fe67c864369b88fdee5
monika1020 <monapatil1020@gmail.com>
2020-08-15 20:26:47 +0530

    Ass10

---
+++ day10/10.2.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+let likeCoutner = 0;
+
+let likeMe = function (btnElement) {
+    console.log(btnElement);
+
+    likeCoutner++;
+    btnElement.innerHTML = "Like " + likeCoutner;
+};
